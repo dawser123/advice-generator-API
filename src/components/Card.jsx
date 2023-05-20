66dc449db51ed9be0a968e36ddf71e2e236d3771
Dawid <dawidserafin96@gmail.com>
2023-05-20 13:39:31 +0200

    Finish react  component development

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import IconDice from '../assets/Icon-dice'
-
 import classes from './Card.module.css'
 import Pattern from '../assets/Pattern-divider-desktop.svg'
+
 const Card = () => {
+	const [advice, setAdvice] = useState({ id: null, text: null })
+	const [isLoading, setIsLoading] = useState(true)
+	const [error, setError] = useState(false)
+	const fetchAdviceHandler = async () => {
+		try {
+			const response = await fetch('https://api.adviceslip.com/advice')
+			if (!response.ok) {
+				throw new Error('Failed to fetch advice.')
+			}
+			const { slip } = await response.json()
+			setAdvice({ id: slip.id, text: slip.advice })
+			setIsLoading(false)
+		} catch (error) {
+			setIsLoading(false)
+			setError(true)
+		}
+	}
+
+	useEffect(() => {
+		fetchAdviceHandler()
+	}, [])
+
 	return (
 		<div className={classes.container}>
-			<h1>advice</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatum incidunt exercitationem ipsum nostrum
-				perspiciatis et doloribus laboriosam sequi asperiores.
-			</p>
-			<img src={Pattern} />
-			<button>
+			{error ? (
+				<p className={classes.error}>Failed to fetch advice.</p>
+			) : (
+				<>
+					<h1>advice #{advice.id}</h1>
+					{isLoading ? <p>Loading advice...</p> : <p>{advice.text}</p>}
+					<img src={Pattern} alt="Pattern" />
+				</>
+			)}
+
+			<button onClick={fetchAdviceHandler}>
 				<IconDice className={classes['icon-dice']} />
 			</button>
 		</div>
